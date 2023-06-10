deb41c1dc84ddd19c13d066bc4529e5540486b0e
“Stephen <stephen.jose@creysto.com>
2023-06-10 14:33:26 +0530

    Stephen Jose, Added getter and setters to access the variable

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        RWOnly obj=new RWOnly();
+        // Cannot access private members
+        // System.out.println(obj.name);
+        obj.setter("Jose");
+        System.out.println(obj.getter());
+    }
+
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+    private String name="Stephen TJ";
+    public String getter(){
+        return this.name;
+    }
+
+    public void setter(String name){
+        this.name=name;
+    }
     
 }
